c63b824f44c4ef10204f4cb186a5979c4242aa81
RRagasa1 <rragasa1@studentmail.nait.ca>
2016-10-12 20:46:00 -0600

    Set up of current Chinook roles to security database

--- PracticeChinook/ChinookSystem/Security/RoleManager.cs
+++ PracticeChinook/ChinookSystem/Security/RoleManager.cs
@@ -7,16 +7,36 @@ using System.Threading.Tasks;
 #region Additional Namespaces 
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity;
+using System.Linq;
 #endregion
 
 
 namespace ChinookSystem.Security
 {
-    public class RoleManager:RoleManager<IdentityRole>
+    public class RoleManager : RoleManager<IdentityRole>
     {
         public RoleManager() : base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
         {
 
+        }
+
+        public void AddChinookRoles()
+        {
+            foreach (string rolename in SecurityRoles.ChinookSecurityRoles)
+            {
+                //check the security tables of the chinook system 
+                //to see if any new roles need to be added to the security table data
+
+                //the first time your application runs, 
+                //all the roles will be added to the security table data
+
+                //all other times, your application runs (start),
+                //only new roles in the ChinookSecurityRoles will be added to the security table data.
+                if (!Roles.Any(r => r.Name.Equals(rolename)))
+                {
+                    this.Create(new IdentityRole(rolename));
+                }
+            }
         }
     }
 }
---
+++ PracticeChinook/ChinookSystem/Security/SecurityRoles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Security
+{
+    internal static class SecurityRoles  //one copy per application
+    {
+        public const string WebsiteAdmins = "WebsiteAdmins";
+        public const string RegisteredUsers = "RegisteredUsers";
+        public const string Staff = "Staff";
+        public const string Auditor = "Auditor";
+        //property that is readOnly which will return all of the roles in the application
+        public static List<string> ChinookSecurityRoles
+        {
+            get
+            {
+                List<string> roleList = new List<string>();
+                //add all of the system roles to the list of roles
+                roleList.Add(WebsiteAdmins);
+                roleList.Add(RegisteredUsers);
+                roleList.Add(Staff);
+                roleList.Add(Auditor);
+
+                return roleList;
+            }
+        }
+
+    }
+}
