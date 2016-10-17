7109d4237f92aa9d06ff0433f63bcd7908e678b7
RRagasa1 <rragasa1@studentmail.nait.ca>
2016-10-17 17:42:55 -0600

    Added Security Folder to component library
    ApplicationUser
    ApplicationDbContext
    UserManager

---
+++ eRestaurant/eRestaurantSystem/Security/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.AspNet.Identity.EntityFramework;
+#endregion
+
+namespace eRestaurantSystem.Security
+{
+    public class ApplicationUser : IdentityUser
+    {
+        //public int? EmployeeId { get; set; }
+        //public int? CustomerId { get; set; }
+    }
+
+}
---
+++ eRestaurant/eRestaurantSystem/Security/UserManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.AspNet.Identity.EntityFramework;    //UserStore, ApplicationDbcontext
+using Microsoft.AspNet.Identity;                    //UserManager
+using System.ComponentModel;                        //ODS
+using eRestaurantSystem.DAL;                            //eRestaurant Context
+using eRestaurantSystem.Data.Entities;                  //Entities
+#endregion
+
+namespace eRestaurantSystem.Security
+{
+    public class UserManager : UserManager<ApplicationUser>
+    {
+        public UserManager()
+            : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+        {
+        }
+    }
+}
